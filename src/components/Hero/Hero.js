c8d295d2cdd79e49c6702a7c44fb35411f0ceb6c
akshaychavanonly <csakshay2000@gmail.com>
2024-08-01 20:10:29 +0530

    image chanhge

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 import "../../styles/Hero/Hero.scss";
 
-import Myself from "/Users/csaks/Downloads/react-portfolio/src/components/image/me.jpg";
+import Myself from "../../assets/images/me.jpg";
 
 const Hero = () => {
   useEffect(() => {
